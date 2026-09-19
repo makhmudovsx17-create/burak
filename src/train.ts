fb7d9c62be249e61478bb114bc7bae8366dc79e1
Sarvarbek <makhmudovsx17@gmail.com>
2026-09-20 07:27:17 +0900

    feat: create train area and MITASK-L

---
+++ src/train.ts
@@ -0,0 +1,18 @@
+function majorityElement(arr: number[]): number {
+    const counts: Record<number, number> = {};
+    let maxCount = 0;
+    let result = arr[0];
+
+    for (const num of arr) {
+        counts[num] = (counts[num] || 0) + 1;
+
+        if (counts[num] > maxCount) {
+            maxCount = counts[num];
+            result = num;
+        }
+    }
+
+    return result;
+}
+
+console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4])); // 4
