798f4b999c0d9f359921d0d42b577c415065a34f
Sarvarbek <makhmudovsx17@gmail.com>
2026-09-22 19:20:29 +0900

    feat: MITASK - M

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,30 @@
-function majorityElement(arr: number[]): number {
-    const counts: Record<number, number> = {};
-    let maxCount = 0;
-    let result = arr[0];
+// MITASK - M
 
-    for (const num of arr) {
-        counts[num] = (counts[num] || 0) + 1;
-
-        if (counts[num] > maxCount) {
-            maxCount = counts[num];
-            result = num;
-        }
-    }
-
-    return result;
+function getSquareNumbers(numbers: number[]): { number: number; square: number }[] {
+    return numbers.map((number) => {
+        return { number: number, square: number * number };
+    });
 }
 
-console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4]));
+console.log(getSquareNumbers([1, 2, 3]));
+
+// MITASK - L
+
+// function majorityElement(arr: number[]): number {
+//     const counts: Record<number, number> = {};
+//     let maxCount = 0;
+//     let result = arr[0];
+
+//     for (const num of arr) {
+//         counts[num] = (counts[num] || 0) + 1;
+
+//         if (counts[num] > maxCount) {
+//             maxCount = counts[num];
+//             result = num;
+//         }
+//     }
+
+//     return result;
+// }
+
+// console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4]));
