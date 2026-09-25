46fd489daff273d66439dbc868ad19f601f95077
Sarvarbek <makhmudovsx17@gmail.com>
2026-09-25 14:43:53 +0900

    feat: develop member schema model

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const routerAdmin = express.Router();
-import restaurantController from "./controllers/reastaurant.controller";
+import restaurantController from "./controllers/restaurant.controller";
 
 routerAdmin.get('/', restaurantController.goHome);
 
